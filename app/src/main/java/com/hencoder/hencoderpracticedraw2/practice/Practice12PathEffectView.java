7c24bf45b31cd245033356b5835139118e0a88c8
lizheng <lizheng@hunlidaheng.com>
2018-10-04 16:39:56 +0800

    作业完成~

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
@@ -2,8 +2,15 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.graphics.SumPathEffect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,6 +18,8 @@ import android.view.View;
 public class Practice12PathEffectView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Path path = new Path();
+    PathDashPathEffect pathDashPathEffect;
+
 
     public Practice12PathEffectView(Context context) {
         super(context);
@@ -33,6 +42,14 @@ public class Practice12PathEffectView extends View {
         path.rLineTo(100, 100);
         path.rLineTo(70, -120);
         path.rLineTo(150, 80);
+
+
+        Path p = new Path();
+        p.lineTo(20, -30);
+        p.lineTo(40, 0);
+        p.close();
+        pathDashPathEffect = new PathDashPathEffect(p, 50,
+                0, PathDashPathEffect.Style.MORPH);
     }
 
     @Override
@@ -42,35 +59,50 @@ public class Practice12PathEffectView extends View {
         // 使用 Paint.setPathEffect() 来设置不同的 PathEffect
 
         // 第一处：CornerPathEffect
+        paint.setPathEffect(new CornerPathEffect(20));
         canvas.drawPath(path, paint);
 
         canvas.save();
         canvas.translate(500, 0);
         // 第二处：DiscretePathEffect
+
+        paint.setPathEffect(new DiscretePathEffect(20, 5));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 200);
         // 第三处：DashPathEffect
+        paint.setPathEffect(new DashPathEffect(new float[]{10, 5}, 0));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 200);
         // 第四处：PathDashPathEffect
+
+        paint.setPathEffect(pathDashPathEffect);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 400);
         // 第五处：SumPathEffect
+
+        PathEffect dashEffect = new DashPathEffect(new float[]{10, 5}, 0);
+        PathEffect discreatePathEffect = new DiscretePathEffect(20, 5);
+        PathEffect sumPathEffect = new SumPathEffect(dashEffect,discreatePathEffect);
+
+        paint.setPathEffect(sumPathEffect);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 400);
         // 第六处：ComposePathEffect
+
+        PathEffect composePathEffect = new ComposePathEffect(dashEffect,discreatePathEffect);
+        paint.setPathEffect(composePathEffect);
         canvas.drawPath(path, paint);
         canvas.restore();
     }
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice13ShadowLayerView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice13ShadowLayerView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -24,13 +25,14 @@ public class Practice13ShadowLayerView extends View {
 
     {
         // 使用 Paint.setShadowLayer() 设置阴影
+        paint.setTextSize(120);
+        paint.setShadowLayer(10,0,0, Color.RED);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        paint.setTextSize(120);
         canvas.drawText("Hello HenCoder", 50, 200, paint);
     }
 }
